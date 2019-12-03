d0b6877fed5d9da612e1a22848c80b6a3dc06c2c
Guillaume <guillaume.cirillo@gmail.com>
2019-12-03 15:10:01 +0100

    Ajout de fonction anonyme et exception

--- all_test/test.php
+++ all_test/test.php
@@ -5,7 +5,7 @@ namespace all_test\guigui;
 abstract class Users {
 
     private $name;
-    protected $pdv;
+    public $pdv;
 
     protected function __construct(string $name, int $pdv) {
         $this->name = $name;
@@ -28,31 +28,77 @@ final class Personnage extends Users implements Armor {
     private $type;
     private $casque;
     private $armure;
-
-    function equipement(int $armure, int $casque){
+    private $arme;
+    
+    public function equipement(int $armure, int $casque) 
+    {
         $this->shield = $casque + $armure;
     }
-    function __construct(string $name, int $pdv, int $type, int $armure, int $casque) {
 
+    public function armes()
+    {
+ 
+        $equipementArme = function ()
+        {
+            echo($this->getName()." à ".$this->arme."<br>");
+        };
+
+        $this->functionEquipementArme($equipementArme);
+    }
+
+    public function functionEquipementArme($equipementArme)
+    {
+        $equipementArme();
+    }
+
+    function __construct(string $name, int $pdv, int $type, int $armure, int $casque, string $arme) 
+    {
+       
         $listTypes = [
             "Guerrier",
             "Archer"
         ];
 
+        $listArme = [
+            "une Épée",
+            "un Arc"
+        ];
+
         $this->type = $listTypes[$type];
         $this->casque = $casque;
         $this->armure = $armure;
+        $this->arme = $listArme[$arme];
 
         $this->equipement($this->armure, $this->casque);
         parent::__construct($name, $pdv);
         echo "Je m'appelle ".$this->getName().", j'ai ".$this->pdv." points de vie ";
         echo "et je suis de type ".$this->type;
-        echo ". Mon point d'armure et de ".$this->shield."<br>";
+        echo ". Mon point d'armure est de ".$this->shield;
+        echo ". Mon arme est ".$this->arme."<br>";
+
     }
+
+  function lifePoint(){
+      if ($this->pdv == 0)
+      {
+        throw new \Exception($this->getName().' n\'a plus de point vie.');
+      }
+      echo $this->getName()." a encore ".$this->pdv." points de vie";
+  }
+
+  function exceptionPdv(){
+        try 
+        {
+            $this->lifePoint();
+        } 
+        catch (\Exception $e) 
+        {
+            echo 'Exception reçue : '.$e->getMessage().'<br>';
+        }
+  } 
+
+
 }
-
-
-
 
 
 
--- all_test/test2.php
+++ all_test/test2.php
@@ -3,15 +3,22 @@ include 'test.php';
 
 use \all_test\guigui;
 
+// (Nom, Point de vie, Classe(0 = Guerrier, 1 = Archer), Point d'armure de l'armure, Point d'armure du casque, Type d'arme(0 = Épée, 1 = Arc))
 
-$personnage1 = new guigui\Personnage("Guillaume", 120, 0, 80, 20);
+$personnage1 = new guigui\Personnage("Guillaume", 0, 0, 80, 20, 0);
 
-$personnage = new guigui\Personnage("Clara",150,1,80,20);
+$personnage = new guigui\Personnage("Clara",150,1,80,20, 1);
 
+$personnage1->armes();
+$personnage->armes();
 
+$personnage1->exceptionPdv();
+$personnage->exceptionPdv();
 
 var_dump($personnage1);
 var_dump($personnage);
+
+
 
 
 ?>
